bf332b4f4497675335df757b1c9e01303dda84a2
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-09-14 07:15:12 +0100

    Rename USer API doc to User Registration API doc

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -4,51 +4,46 @@ import sanitizeRoute from "../middleware/sanitizeRoute";
 
 const userRouter = Router();
 
-// userSchema doc
 /**
  * @swagger
- * components:
- *   schemas:
- *     User:
- *       type: object
- *       required:
- *         - username
- *         - email
- *         - password
- *         - passwordConfirmation
- *       properties:
- *          username:
- *            type: string
- *            description: Username of the user (unique)
- *          email:
- *            type: string
- *            description: Email of the user (unique)
- *          password:
- *             type: string
- *             description: Password of the user (must be a alpha-numeric value with at least one special character and have a min length of 8 characters)
- *          passwordConfirmation: same as password
- *       example:
- *         username: johndoe
- *         email: johndoe@example.com
- *         password: A very secure password
- *         passwordConfirmation: A very secure password
+ * tags:
+ *   name: User Registration
+ *   description: User registration API
  */
 
 /**
  * @swagger
- * tags:
- *   name: Users
- *   description: Users API
  * /user/signup:
  *   post:
  *     summary: Sign up a user
- *     tags: [Users]
+ *     tags: [User Registration]
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: "#/components/schemas/User"
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *               - passwordConfirmation
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Username of the user (unique)
+ *               email:
+ *                 type: string
+ *                 description: Email of the user (unique)
+ *               password:
+ *                 type: string
+ *                 description: Password of the user (must be a alpha-numeric value with at least one special character and have a min length of 8 characters)
+ *               passwordConfirmation: same as password
+ *             example:
+ *               username: johndoe
+ *               email: johndoe@example.com
+ *               password: A very secure password
+ *               passwordConfirmation: A very secure password
  *     responses:
  *       200:
  *         description: The created user
@@ -66,13 +61,10 @@ userRouter.post("/signup", sanitizeRoute, userController.signup);
 
 /**
  * @swagger
- * tags:
- *   name: Users
- *   description: Users API
  * /user/login:
  *   post:
  *     summary: Login a user
- *     tags: [Users]
+ *     tags: [User Registration]
  *     requestBody:
  *       required: true
  *       content:
